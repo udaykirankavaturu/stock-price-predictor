00a77efe7acdad5be055955e4a35437ef09a42bc
Uday Kiran Kavaturu <udaykiran.kavaturu@xcubelabs.com>
2023-03-18 04:23:37 +0530

    up to flask app successful deployment

--- Dockerfile
+++ Dockerfile
@@ -3,4 +3,4 @@ WORKDIR /app
 COPY . /app
 RUN pip3 install --no-cache-dir -r requirements.txt
 EXPOSE 5000
-CMD python3 app.py
+CMD python3 ./app.py
--- app.py
+++ app.py
@@ -1,6 +1,13 @@
-from flask import Flask
+from flask import Flask, make_response
 
 app = Flask(__name__)
+
+
+@app.route('/health-check')
+def index():
+    response = make_response("This is a response with a 200 status code")
+    response.status_code = 200
+    return response
 
 
 @app.route('/')
@@ -9,4 +16,4 @@ def hello_world():
 
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=6001)
+    app.run(host='0.0.0.0')
